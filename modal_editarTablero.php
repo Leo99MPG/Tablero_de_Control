bb634188874335396813fe06d498b4efd03eb3ce
Mezapleo99@gmail.com <Mezapleo99@gmail.com>
2025-03-28 15:20:17 -0600

    Cambios de diseño menores

--- modal_editarTablero.php
+++ modal_editarTablero.php
@@ -10,7 +10,7 @@
 <div class="container">
         <div class="modal fade" id="modal_editTablero" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
-                <div class="modal-content">
+                <div class="modal-content bs-success-bg-subtle">
                     <div class="modal-header">
                         <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Tablero</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
